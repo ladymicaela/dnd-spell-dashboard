bca7c67dedd9e6bcb07728284fe3ea26c0831683
Micaela Moss <mcmoss1991@gmail.com>
2020-03-10 13:29:20 -0700

    Change spell slot render function to be more dynamic

--- frontend/components/spell_slots.jsx
+++ frontend/components/spell_slots.jsx
@@ -5,16 +5,30 @@ class SpellSlots extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            0: [],
-            1: [],
-            2: [],
-            3: [],
-            4: [],
-            5: [],
-            6: [],
-            7: [],
-            8: [],
-            9: []
+            spells: {
+                0: ['','','','','','',''],
+                1: ['', '', '', '', '', ''],
+                2: ['', '', '', '', '', ''],
+                3: ['', '', '', '', '', ''],
+                4: ['', '', '', '', '', ''],
+                5: ['', '', '', '', '', ''],
+                6: ['', '', '', '', '', ''],
+                7: ['', '', '', '', '', ''],
+                8: ['', '', '', '', '', ''],
+                9: ['', '', '', '', '', '']
+            },
+            slots: {
+                0: [],
+                1: [true, true, true, true],
+                2: [true, true, true],
+                3: [true, true, true],
+                4: [true, true, true],
+                5: [true, true, true],
+                6: [true, true],
+                7: [true, true],
+                8: [true],
+                9: [true]
+            }
         }
 
     };
@@ -26,150 +40,34 @@ class SpellSlots extends React.Component {
     render() {
         return (
             <div className="spell-slots-container">
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Cantrips</h3>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 1</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 2</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 3</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 4</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 5</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 6</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 7</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 8</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
-                <div className="spell-level">
-                    <h3 className="spell-level-header">Spell Level: 9</h3>
-                    <div className="spell-slots">
-                        <input type="checkbox"/>
-                    </div>
-                    <div className="spells">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                    </div>
-                </div>
+                {
+                    Object.keys(this.state.spells).map( (spell, idx) => {
+                        return (
+                            <div className="spell-level" key={idx}>
+                                <h3 className="spell-level-header">Spell Level: {spell}</h3>
+                                <div className="spell-slots">
+                                    {this.state.slots[spell].map( (slot, idx) => {
+                                        return (
+                                            <input type="checkbox" key={idx}/>
+                                        )
+                                    })}
+                                </div>
+                                <div className="spells">
+                                    {
+                                        this.state.spells[spell].map( (name, idx) => {
+                                            return (
+                                                <input type="text" value={name} key={idx}/>
+                                            )
+                                        })
+                                    }
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
             </div>
-        )
-    }
+    )}
 
 
 };
